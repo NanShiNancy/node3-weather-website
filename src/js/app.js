4a6599cba50d5153d3c9646ceb15527083f40d3c
nancyshi <iamnancyshi@hotmail.com>
2020-04-14 15:27:44 +1000

    Init commit

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+// import { response } from "express"
+
+console.log('Client side javascript file is loaded')
+
+fetch('http://localhost:3000/weather/?address=brisbane').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecast)
+            }
+    })
+    
+})
+
